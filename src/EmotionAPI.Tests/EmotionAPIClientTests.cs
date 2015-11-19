5c6aed889194147895c53baa910e057990a26fd5
Adam Hilton <adam.tyler.hilton@gmail.com>
2015-11-19 10:13:03 -0600

    Split test into two separate tests

--- src/EmotionAPI.Tests/EmotionAPIClientTests.cs
+++ src/EmotionAPI.Tests/EmotionAPIClientTests.cs
@@ -5,13 +5,19 @@ namespace EmotionAPI.Tests
     public class EmotionAPIClientTests : EmotionAPITestsBase
     {
         [Fact]
-        public void Can_Create_EmotionAPIClient_Controller()
+        public void Can_Create_EmotionAPIClient_Class()
         {
-            var controller = new EmotionAPIClient(mockOcpApimSubscriptionKey);
+            var sut = new EmotionAPIClient(mockOcpApimSubscriptionKey);
             
-            Assert.NotNull(controller);
-            Assert.NotEmpty(controller.OcpApimSubscriptionKey);
+            Assert.NotNull(sut);
         }
-        
+
+        [Fact]
+        public void OcpApimSubscriptionKey_Is_Being_Set()
+        {
+            var sut = new EmotionAPIClient(mockOcpApimSubscriptionKey);
+
+            Assert.NotEmpty(sut.OcpApimSubscriptionKey);
+        }
     }
 }
